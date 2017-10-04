c065f14fc81489e6480d8dfcb0c20f64ec88c7d4
Sergey Zalozniy <sergey.zalozniy@interpretator.com>
2017-10-04 15:17:07 +0300

    Initial Commit

---
+++ ZCDebug/ZCDebug.h
@@ -0,0 +1,19 @@
+//
+//  ZCDebug.h
+//  ZCDebug
+//
+//  Created by Sergey Zalozniy on 04.10.17.
+//  Copyright © 2017 ihsuropu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZCDebug.
+FOUNDATION_EXPORT double ZCDebugVersionNumber;
+
+//! Project version string for ZCDebug.
+FOUNDATION_EXPORT const unsigned char ZCDebugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZCDebug/PublicHeader.h>
+
+
